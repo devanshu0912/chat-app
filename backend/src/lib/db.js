8e140699f4b2cbaa94f450a5ae359b727f00539b
devanshu09 <dshukla1234fz@gmail.com>
2025-09-08 16:43:50 +0530

    basic setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,11 @@
+import moongoose from "mongoose";
+
+export const connectDB=async()=>{
+    try {
+        const conn=await moongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected : ${conn.connection.host}`);
+    } catch (error) {
+        console.log("mongoose  connection Error:${error.message:",error);
+        
+    }
+};
