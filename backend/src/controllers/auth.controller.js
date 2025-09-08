d3ac95a174b4bc272b8c313d67ea1d407a54341c
devanshu09 <dshukla1234fz@gmail.com>
2025-09-08 18:48:11 +0530

    login  and logout (api tested)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const signup = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-
+ 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
