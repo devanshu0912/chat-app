2bd320c34dfb4be9457cd96b54ed15f84427c6bb
devanshu09 <dshukla1234fz@gmail.com>
2025-12-14 22:50:41 +0530

    localhost to vitebackend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,14 @@
-// instance that we can use throughout our app
-import axios from "axios";
-export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5003/api",
-    withCredentials:true,
+// // instance that we can use throughout our app
+// import axios from "axios";
+// export const axiosInstance = axios.create({
+//     baseURL:"http://localhost:5003/api",
+//     withCredentials:true,
 
+// });
+
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  withCredentials: true,
 });
